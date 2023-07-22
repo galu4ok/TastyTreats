2642f0223437edd2eca439cb3fda5e135c7e3554
galu4ok <galu4ok@gmail.com>
2023-07-22 10:45:53 +0000

    Deploying to gh-pages from @ galu4ok/TastyTreats@45d4a0ddc03c4ca6d91cf3a19e82c17dd0f847b4 🚀

--- index.2ace6dc0.js
+++
@@ -1,2 +0,0 @@
-!function(){var e="undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{},n={},o={},r=e.parcelRequired7c6;null==r&&((r=function(e){if(e in n)return n[e].exports;if(e in o){var r=o[e];delete o[e];var i={id:e,exports:{}};return n[e]=i,r.call(i.exports,i,i.exports),i.exports}var d=new Error("Cannot find module '"+e+"'");throw d.code="MODULE_NOT_FOUND",d}).register=function(e,n){o[e]=n},e.parcelRequired7c6=r),r("d3Gjh"),r("cs7FV")}();
-//# sourceMappingURL=index.2ace6dc0.js.map
